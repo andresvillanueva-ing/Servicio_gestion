4e95655e26142b8c69ce5b7d6b1a3b66d32f85f5
andresvillanueva-ing <andresvillanuevaricaurte7@gmail.com>
2025-05-06 21:50:29 -0500

    confirmacion

---
+++ views/InformacionServicios.py
@@ -0,0 +1,8 @@
+from kivymd.uix.screen import MDScreen
+
+
+class informacion_servicios_screen(MDScreen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.name = "informacionservicios"
+
---
+++ views/ReservasScreen.py
@@ -0,0 +1,7 @@
+from kivymd.uix.screen import MDScreen
+
+
+class reservas_screen(MDScreen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.name = "reservasscreen"
