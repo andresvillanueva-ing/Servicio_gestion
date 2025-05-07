223b723c42f8d479b6e071379b43a894a28fc801
andresvillanueva-ing <andresvillanuevaricaurte7@gmail.com>
2025-05-06 21:01:12 -0500

    tabla reservas

--- Database/Data_Reservas.py
+++ Database/Data_Reservas.py
@@ -21,3 +21,28 @@ def agregar_reserva(razon_social, nit, administrador, ubicacion, tipo_servicio, 
     cursor.close()
     conexion.close()
 
+def obtener_reservas_realizadas():
+    conexion = crear_conexion()
+    cursor = conexion.cursor()
+    sql = "SELECT razon_social, nit, administrador, ubicacion, tipo_servicio, imagen, nombre_cliente, telefono_cliente, correo_cliente, hora_reserva, fecha_reserva FROM data_reservas WHERE id = %s"
+    cursor.execute(sql, (id,))
+    reservas = cursor.fetchall()
+    cursor.close()
+    conexion.close()
+
+    return[
+        {
+            "razon_social": fernet.decrypt(row[0]).decode(),
+            "nit": fernet.decrypt(row[1]).decode(),
+            "administrador": fernet.decrypt(row[2]).decode(),
+            "ubicacion": row[3],
+            "tipo_servicio": fernet.decrypt(row[4]).decode(),
+            "imagen": row[5],
+            "nombre_cliente": fernet.decrypt(row[6]).decode(),
+            "telefono_cliente": fernet.decrypt(row[7]).decode(),
+            "correo_cliente": fernet.decrypt(row[8]).decode(),
+            "hora_reserva": row[9],
+            "fecha_reserva": row[10],
+        }
+        for row in reservas
+    ]
